70c5cda5338d3a70031f218f25917b787fe4ad45
Hiago Pinheiro <devj4g0@gmail.com>
2024-01-15 13:53:53 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1 align="center"> Detona Ralph Game</h1>
+<h2 align="center">Uma versão implementada do jogo Detona Ralph da DIO.</h2>
+
+<p>É com muito orgulho que compartilho com vocês o meu primeiro jogo e projeto envolvendo minhas primeiras linguagens de programação.</p>
+
+### 🛠 Tecnologias
+
+As seguintes ferramentas foram usadas na construção do projeto:
+
+- [Visual Studio Code](<https://code.visualstudio.com/>)
+- [Node.js](<https://nodejs.org/en/>)
+- [JavaScript](<https://developer.mozilla.org/pt-BR/docs/Web/JavaScript>)
+- [HTML](<https://developer.mozilla.org/pt-BR/docs/Learn/HTML>)
+- [CSS](<https://developer.mozilla.org/pt-BR/docs/Learn/CSS>)
+
+O jogo é inspirado no filme "Detona Ralph". O seu desenvolvimento é uma das tarefas do Bootcamp Potência Tech iFood - Desenvolvimento de Jogos.
+
+### implementações: <br>
+<ol>
+<li>Movimentação do inimigo em um intervalo aleatório e inconstante</li>
+<li>Redução da vida ao errar um golpe.</li>
+<li>Implementação de um cursor do mouse personalizado.</li>
+<li>Implementação de áudio nos eventos handleMiss, handleHit e gameOver.</li>
+<li>Ajuste da imagem do inimigo para ser exibido por inteiro.</li>
+</ol>
+
+### Créditos
+
+Este jogo foi desenvolvido como parte de um projeto educacional da Digital Innovation One.
+---
+Fiquem a vontade em contribuírem com novas ideias, dar fork e se gostarem do projeto, deixem uma ⭐.
---
+++ src/scripts/engine.js
@@ -0,0 +1,122 @@
+document.body.style.cursor = 'none';
+
+let isMouseDown = false;
+
+document.addEventListener('mousemove', (event) => {
+    if (!isMouseDown) {
+        document.body.style.setProperty('--cursorX', `${event.clientX - 8}px`);
+        document.body.style.setProperty('--cursorY', `${event.clientY - 8}px`);
+    }
+});
+
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+        lives: document.querySelector("#lives"),
+    },
+    values: {
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+        currentLife: 5,
+        loopTimeoutId: null,
+    },
+
+    actions: {
+        countDownTimerId: setInterval(countDown, 1000),
+    },
+    audio: {
+        skip: "skip.wav",
+        explosionGoo: "sfx_explosionGoo.ogg",
+        miss: "miss.ogg",
+        gameOver: "game-over.wav",
+    },
+};
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.values.loopTimeoutId);
+        playSound(state.audio.gameOver);
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+}
+
+function playSound(audioName) {
+    let audio = new Audio(`./audio/${audioName}`);
+    audio.volume = 0.5;
+
+    audio.addEventListener('error', function (e) {
+        console.error('Erro ao carregar o áudio');
+    });
+
+    audio.play();
+}
+
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+    playSound(state.audio.skip);
+}
+
+function startGameLoop() {
+    function loop() {
+        var skipTime = Math.round((Math.random() * 1300) + 250);
+        state.values.loopTimeoutId = setTimeout(function () {
+            randomSquare();
+            loop();
+        }, skipTime);
+    }
+
+    loop();
+}
+
+function handleHit() {
+    state.values.result++;
+    state.view.score.textContent = state.values.result;
+    playSound(state.audio.explosionGoo);
+}
+
+function handleMiss() {
+    if (state.values.currentLife > 1) {
+        state.values.currentLife--;
+        state.view.lives.textContent = state.values.currentLife;
+        playSound(state.audio.miss);
+    } else {
+        state.values.currentLife--;
+        playSound(state.audio.gameOver);
+        clearInterval(state.values.loopTimeoutId);
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+}
+
+function addListenerHitbox() {
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition) {
+                handleHit();
+            } else {
+                handleMiss();
+            }
+        });
+    });
+}
+
+function initialize() {
+    addListenerHitbox();
+    startGameLoop();
+}
+
+initialize();
